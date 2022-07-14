801715f6eb50dc6bb9da17862ae4e12c3d0ae09c
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-14 14:37:12 +0200

    init

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,24 +1,24 @@
 
-// LEV_1.1
-function myFunction() {
-    document.getElementById("test").style.color = "red";
-}
+console.log('Hello!')
 
-// LEV_1.2
+let carName = 'BMW'
+console.log('carName')
 
-// This is a single line JavaScript comment
-document.write("I have comments in my JavaScript code!");
-//document.write("You can't see this!");
-//-->
+let x = 150
+console.log('x')
 
-document.write("I have multi-line comments!");
-/*document.write("You can't see this!");
-document.write("You can't see this!");
-document.write("You can't see this!");
-document.write("You can't see this!");
-document.write("You can't see this!");
-document.write("You can't see this!");
-document.write("You can't see this!");*/
+let y = 50
+console.log('y')
 
-// LEV_1.7
+let z = x + y
+console.log('z')
 
+let firstName, lastName, age
+firstName = 'John'
+console.log('firstName')
+
+lastName = 'Doe'
+console.log('lastName')
+
+age = 35
+console.log('age')
