eae47105ae7133d0bbc1a69a13bf9d9321316abe
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-14 14:26:21 +0200

    LEV_1.5 veriables

---
+++ assets/JS/main.js
@@ -0,0 +1,24 @@
+
+// LEV_1.1
+function myFunction() {
+    document.getElementById("test").style.color = "red";
+}
+
+// LEV_1.2
+
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+// LEV_1.7
+
